4e0fc517e427f396f2531a3d51fda65a06889f23
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-19 16:22:48 +0530

    Send mail - DONE | ED-Tech

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mailsender = require('../utils/mailSender');
 
 const OTPSchema = new mongoose.Schema({
     email:{
@@ -16,6 +17,24 @@ const OTPSchema = new mongoose.Schema({
         default:Date.now(),
         expires: 5*60,
     }
+});
+
+// Function to send mail
+
+async function sendVerificationEmail(email, otp){
+    try {
+        const mailResponse = await mailsender(email,"Verification Email from StudyNotion", otp);
+        console.log("Email send successfully -> ", mailResponse);
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+// Before saving doc, send the verification email with current object data (this.);
+OTPSchema.pre("save", async function(next){
+    await sendVerificationEmail(this.email, this.otp);
+    next();
 });
 
 module.exports = mongoose.model("OTP",OTPSchema);
