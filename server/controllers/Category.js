52c7c6b7798355dda9e3816a578e0f170f99d08f
omkhodse18 <omkarkhodse.18@gmail.com>
2024-04-23 12:30:18 +0530

    Categories added | Ed-Tech

---
+++ server/controllers/Category.js
@@ -0,0 +1,57 @@
+const Category = require('../models/Category');
+
+// createCategory
+exports.createCategory = async(req, res) => {
+    try {
+        // fetch data
+        const {name, description} = req.body;
+
+        // Validation
+        if(!name || !description){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required."
+            })
+        }
+
+        //create entry in Tag db
+        const categoryDetails = await Category.create({
+            name: name,
+            description: description
+        });
+
+        return res.status(200).json({
+            success:true,
+            message:"Category Created successfully"
+        })
+
+
+    } catch (error) {
+        console.log(error);
+
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
+
+// showAllCategory
+exports.showAllCategory = async(req, res) => {
+    try {
+        // We dont parameter to find, but make sure all entry should have name, description
+        const allCategory = await Category.find({},{name:true, description:true});
+
+        return res.status(200).json({
+            success:true,
+            message:"All tags are returned successfully",
+            allCategory
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+}
