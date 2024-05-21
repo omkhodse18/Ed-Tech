9fb661ec1bc80592d037eaa59f21d47553f37acf
omkhodse18 <omkarkhodse.18@gmail.com>
2024-05-21 11:17:18 +0530

    Category Page Details - Done | Ed-Tech

--- server/controllers/Category.js
+++ server/controllers/Category.js
@@ -56,4 +56,43 @@ exports.showAllCategory = async(req, res) => {
     }
 }
 
+// categoryPageDetails
+exports.categoryPageDetails = async(req, res) => {
+    try {
+        // get category id
+        const {categoryId} = req.body;
 
+        // get courses for specified id
+        const selectedCategory = await Category.findById(categoryId).populate("courses").exec();
+
+        // validation
+        if(!selectedCategory){
+            return res.status(404).json({
+                success:false,
+                message:"Data not found"
+            })
+        }
+
+        // get courses for different category
+        const differentCategories = await Category.find({_id: {$ne: categoryId}})
+                                    .populate("courses").exec(); // ne = not equal
+
+        // get top selling courses - pending
+
+        // return res
+        return res.status(200).json({
+            success:true,
+            data:{
+                selectedCategory,
+                differentCategories
+            },
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        });
+    }
+}
